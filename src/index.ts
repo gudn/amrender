59afb297eea9ab8466a3206d22ba744c996463eb
Daniil Novoselov <gudnmail@gmail.com>
2021-12-05 15:09:54 +0500

    feat: add am-parse

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import config from 'config'
 import { Telegraf } from 'telegraf'
+import { convert_, OutputFormat } from 'am-parse'
+
+const amToTex = (s: string) => convert_(s, OutputFormat.Latex, '')
 
 const bot = new Telegraf(config.get('bot.token'))
 
@@ -7,4 +10,10 @@ process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 bot.start(ctx => ctx.reply('mathzz'))
+bot.on('text', ctx => {
+  const am = ctx.message.text
+  const tex = amToTex(am)
+  ctx.reply(tex)
+})
+
 bot.launch()
